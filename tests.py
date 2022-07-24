90f0d17464e189dff3e6ae7289e693b8d96ae41d
L-meck <Kanaidavid3@gmail.com>
2022-07-24 16:02:18 +0300

    dependency injection issue

--- tests.py
+++ tests.py
@@ -1,4 +1,37 @@
-path = "C:\Users\kanai\webAppsProjects\gachie\assets\lab.csv"
+import csv
+from hashlib import new
+from operator import ne
+
+path = "C:\\Users\\kanai\\webAppsProjects\\gachie\\assets\\lab.csv"
 lines = [line for line in open(path)]
 
-lines[1]
+# print(lines[0])
+# print(lines[2])
+
+print('\n')
+print(lines[2])
+print(lines[2].strip())
+print(lines[2].strip().split(','))
+
+print('\n')
+
+
+# print(dir(csv))
+
+#USING CSV MODULE
+print('USING CSV MODULE')
+
+file = open(path, newline='')
+reader = csv.reader(file)
+
+header = next(reader) #the first line is a header
+data = [row for row in reader] #remaining of the data
+
+print(header)
+print(data[2])
+
+print('\n')
+
+# print(data) #ALL DATA
+
+
